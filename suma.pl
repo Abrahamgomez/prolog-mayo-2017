17289635cd86358ac28f1e8f080374bbde5540ea
Abrahamgomez <abraham.gomez.perez@hotmail.com>
2017-05-18 10:20:27 -0500

    hola en go

---
+++ suma.pl
@@ -0,0 +1,4 @@
+suma(Sumando1, Sumando2, Resultado):- Resultado is Sumando1 + Sumando2.
+resta(Resta1, Resta2, Resultado):- Resultado is Resta1 - Resta2.
+mulp(Multi1, Multi2, Resultado):- Resultado is Multi1 * Mult2.
+div(Div1, Div2, Resultado):- Resultado is Div1 / Div2.
